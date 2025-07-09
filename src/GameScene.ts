853dd618006ab9cb7fe8f9b3a910f6f9669c965b
blazejzj <opexrrator@gmail.com>
2025-07-09 15:15:31 +0200

    feat: coord showing, starting with tileset, we have grass and rocks with collisions!

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -4,6 +4,7 @@ export default class GameScene extends Phaser.Scene {
     player!: Phaser.Physics.Arcade.Sprite;
     cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
     currentDirection!: "up" | "down" | "left" | "right";
+    coordText!: Phaser.GameObjects.Text;
 
     constructor() {
         super("GameScene");
@@ -22,25 +23,47 @@ export default class GameScene extends Phaser.Scene {
             frameWidth: 32,
             frameHeight: 32,
         });
-
         this.load.spritesheet("playerRight", "assets/playerRight.png", {
             frameWidth: 32,
             frameHeight: 32,
         });
+
+        this.load.image("tiles", "assets/firsttilemap.png");
+        this.load.tilemapTiledJSON("map", "assets/map.json");
     }
 
     create() {
-        this.player = this.physics.add.sprite(100, 100, "playerDown");
+        const map = this.make.tilemap({ key: "map" });
+        const tileset = map.addTilesetImage("tileset-first-test", "tiles");
+        const groundLayer = map.createLayer("Tile Layer 1", tileset, 0, 0);
+        const rocksLayer = map.createLayer("Tile Layer 2", tileset, 0, 0);
+        rocksLayer.setCollisionByProperty({ collides: true });
+
+        this.physics.world.setBounds(
+            0,
+            0,
+            map.widthInPixels,
+            map.heightInPixels
+        );
+
+        this.player = this.physics.add.sprite(32 * 10, 32 * 10, "playerDown");
         this.currentDirection = "down";
         this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, rocksLayer);
 
         // camera on the player
         this.cameras.main.setZoom(2);
         this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(
+            0,
+            0,
+            map.widthInPixels,
+            map.heightInPixels
+        );
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        // animate walking
         this.anims.create({
             key: "walkRight",
             frames: this.anims.generateFrameNumbers("playerRight", {
@@ -77,6 +100,13 @@ export default class GameScene extends Phaser.Scene {
             frameRate: 6,
             repeat: -1,
         });
+
+        this.coordText = this.add.text(16, 16, "", {
+            font: "12px helvetica",
+        });
+        this.coordText.setScrollFactor(0);
+        this.coordText.setDepth(1000);
+        this.coordText.setPosition(320, 180);
     }
 
     update() {
@@ -139,19 +169,21 @@ export default class GameScene extends Phaser.Scene {
             this.player.anims.stop();
             this.player.setFrame(0);
         }
-
         if (this.currentDirection === "down" && isMoving) {
             this.player.anims.play("walkDown", true);
         } else if (this.currentDirection === "down" && !isMoving) {
             this.player.anims.stop();
             this.player.setFrame(0);
         }
-
         if (this.currentDirection === "left" && isMoving) {
             this.player.anims.play("walkLeft", true);
         } else if (this.currentDirection === "left" && !isMoving) {
             this.player.anims.stop();
             this.player.setFrame(0);
         }
+
+        this.coordText.setText(
+            `X: ${Math.floor(this.player.x)}\nY: ${Math.floor(this.player.y)}`
+        );
     }
 }
---
+++ tileset-first-test.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset-first-test" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="public/assets/firsttilemap.png" width="32" height="64"/>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
